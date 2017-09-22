3e20aef7511496b2d3ad56304f140f42096643b3
lisbeth <lisbecg@gmail.com>
2017-09-22 14:24:55 -0400

    Adding config info.

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+        uri: 'mongodb://admin:admin123@ds119064.mlab.com:19064/cen3031_bootcamp', //place the URI of your mongo database here.
+    },
+    googleMaps: {
+        key: 'AIzaSyCNPTLgtq-jizVIPgcVrrsazmZbi2qPeKE'
+    },
+    port: 8080
+};
